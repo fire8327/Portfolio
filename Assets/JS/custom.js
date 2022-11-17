97286a8fb10dba4f1086f1e0fd5d42d7117f1fa7
fire8327 <114702513+fire8327@users.noreply.github.com>
2022-11-17 23:04:38 +0300

    Add files via upload

---
+++ Assets/JS/custom.js
@@ -0,0 +1,21 @@
+/* navigation */
+/*jshint esnext: true */
+const toggler = document.getElementById("toggler");
+const mNav = document.getElementById("mobile__nav");
+const close = document.getElementById("toggler__expanded");
+const overlay = document.getElementById("overlay");
+
+toggler.addEventListener("click", () => {
+  mNav.classList.remove("translate-x-full");
+  overlay.classList.remove("hidden");
+});
+
+close.addEventListener("click", () => {
+  mNav.classList.add("translate-x-full");
+  overlay.classList.add("hidden"); 
+});
+
+overlay.addEventListener("click", () => {
+  mNav.classList.add("translate-x-full");
+  overlay.classList.add("hidden"); 
+});
---
+++ Assets/JS/tail.js
@@ -0,0 +1,20 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {            
+            'bgc': '#12121C',
+          },
+        },
+          screens: {
+            'ml':'425px',
+            'sm': '640px',
+            'md': '768px',
+            'lg': '1024px',
+            'xl': '1280px',
+            '2xl': '1536px',
+          }
+    },
+  };
+
+
+  
