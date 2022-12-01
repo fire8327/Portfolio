ce33261677a4e998fecc71d99a86870f55d1e485
fire83274 <fire83274@gmail.com>
2022-12-02 01:18:29 +0300

    create back to top

--- Assets/JS/custom.js
+++ Assets/JS/custom.js
@@ -24,6 +24,20 @@ overlay.addEventListener("click", () => {
   body.classList.remove("overflow-hidden"); 
 });
 
+/* back to top */
+var toTopButton = document.getElementById("to-top-button");
+window.onscroll = function () {
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+        toTopButton.classList.remove("hidden");
+    } else {
+        toTopButton.classList.add("hidden");
+    }
+}
+
+function goToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+}
+
 
 /* Atropos */
 const myAtropos = Atropos({
