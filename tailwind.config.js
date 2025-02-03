54b0e7e32ee2736fa1297274720df7d4b58b71e9
fire8327 <fire83274@gmail.com>
2025-02-03 10:32:49 +0300

    upd css

---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+module.exports = {
+    mode: 'jit',
+    purge: {
+      content: [
+        './components/**/*.{vue,js}',
+        './layouts/**/*.vue',
+        './pages/**/*.vue',
+        './app.vue',
+      ],
+      options: {
+        safelist: ['dark', 'animate-text', 'scroll']
+      }
+    },
+    theme: {
+      extend: {
+        colors: {
+          'light-bg': '#f9f9f9',
+          'dark-bg': '#131313',
+          'light-text': '#f9f9f9',
+          'dark-text': '#131313',
+          'purple': 'rgb(123, 31, 162)',
+          'violet': 'rgb(103, 58, 183)',
+          'pink': 'rgb(244, 143, 177)'
+        },
+        spacing: {
+          'widthContent': '1280px',
+          'widthContentMargin': '16px'
+        },
+        fontFamily: {
+          Inter: ['Inter', 'sans-serif']
+        },
+        backgroundImage: {
+          'gradient-brand': 'linear-gradient(to right, #ec4899, #8b5cf6)'
+        }
+      }
+    }
+  }
