8a77d2d6dfa6260cdd0753a7d185db6af2b9f416
fire8327 <fire83274@gmail.com>
2026-02-09 20:25:55 +0300

    feat: add tailwind

---
+++ app/layouts/default.vue
@@ -0,0 +1,9 @@
+<template>
+    <!-- <Header/> -->
+    <main class="grid-container gap-y-16 lg:gap-y-20 py-8 md:py-12 lg:py-16">
+        <NuxtPage/>
+    </main>
+    <!-- <Footer/>
+    <Cursor/>
+    <Follow/> -->
+</template>
---
+++ app/pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+        <p>123</p>
+    </div>
+</template>
+
+<script setup>
+
+</script>
