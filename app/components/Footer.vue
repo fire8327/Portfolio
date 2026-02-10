ded25c9153a9b50dc86ad83c579933dd023ac257
fire8327 <fire83274@gmail.com>
2026-02-11 00:54:00 +0300

    feat: footer, remove animate-text

---
+++ app/components/Footer.vue
@@ -0,0 +1,6 @@
+<template>
+    <footer class="flex items-center justify-center text-center py-4 md:py-6 border-t dark:border-[#f9f9f9]/30 border-[#131313]/30 gap-1.5 mt-auto">
+        <p class="opacity-30">© 2026 Сайт-портфолио by </p>
+        <NuxtLink to="https://t.me/fire83274" target="_blank" class="text-transparent bg-gradient-to-r from-[#ff7a18] via-[#af002d] to-[#319197] bg-clip-text">fire83274</NuxtLink>
+    </footer>
+</template>
--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -3,7 +3,7 @@
     <main class="grid-container gap-y-16 pb-8 md:pb-12 lg:pb-16">
         <NuxtPage/>
     </main>
-    <!-- <Footer/>
-    <Cursor/>
+    <Footer/>
+    <!-- <Cursor/>
     <Follow/> -->
 </template>
